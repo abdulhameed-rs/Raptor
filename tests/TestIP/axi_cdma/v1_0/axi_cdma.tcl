d6119c7ddb335295985098fa3b4140425c9dd4f3
volodymyrkochyn <vovakochun@gmail.com>
2024-06-27 16:29:58 +0300

    Add new tcl command - add_ip_design

--- tests/TestIP/axi_cdma/v1_0/axi_cdma.tcl
+++ tests/TestIP/axi_cdma/v1_0/axi_cdma.tcl
@@ -12,8 +12,7 @@ configure_ip axi_cdma_v1_0 -mod_name axi_cdma_wrapper -Pdata_width=64 -Paddr_wid
 ipgenerate
 
 #Add generate IP as a design and synthesize it
-add_design_file  $IP_PATH/rapidsilicon/ip/axi_cdma/v1_0/axi_cdma_wrapper/src/axi_cdma_wrapper_v1_0.v
-add_library_path $IP_PATH/rapidsilicon/ip/axi_cdma/v1_0/axi_cdma_wrapper/src/
+add_ip_design axi_cdma_wrapper
 set_top_module axi_cdma_wrapper
 parser_type surelog
 synth_options -new_tdp36k
